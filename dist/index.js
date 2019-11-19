ef83d93317e680b05cb0926a6a3e42d6f1b094cc
zhangjunqiu <zhangjunqiu@beisen.com>
2019-11-19 20:53:07 +0800

    add(all)index

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,11 @@
-module.exports = function({ types: babelTypes }) {
+"use strict";
+
+module.exports = function (_ref) {
+  var babelTypes = _ref.types;
   return {
     name: "add-key-for-each-child",
     visitor: {
-      Identifier(path, state) {
+      Identifier: function Identifier(path, state) {
         if (path.node.name === 'bad') {
           path.node.name = 'good';
         }
