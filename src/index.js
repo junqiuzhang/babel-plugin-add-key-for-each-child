3f8e52cbd471c9ec9e33dae1e54d6c07b129afb3
zhangjunqiu <zhangjunqiu@beisen.com>
2019-11-20 18:55:01 +0800

    fix(all)优化2

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ module.exports = function(babel) {
         var hasMap = path.node && path.node.callee && path.node.callee.property && path.node.callee.property.name === 'map';
         var hasJsx = path.node && path.node.arguments && path.node.arguments[0] && path.node.arguments[0].body && path.node.arguments[0].body.type === 'JSXElement';
         if (hasMap && hasJsx) {
-          path.node.arguments[0].params.push(t.identifier('i'));
-          path.node.arguments[0].body.openingElement.attributes.push(t.jsxAttribute(t.jsxIdentifier('key'), t.jsxExpressionContainer(t.identifier('i'))));
+          var params = path.node.arguments[0].params;
+          var attrib = path.node.arguments[0].body.openingElement.attributes;
+          if (params.length < 2) {
+            path.node.arguments[0].params.push(t.identifier('i'));
+          }
+          if (attrib.findIndex(a => a.name && a.name.name === 'key') < 0) {
+            path.node.arguments[0].body.openingElement.attributes.push(t.jsxAttribute(t.jsxIdentifier('key'), t.jsxExpressionContainer(t.identifier('i'))));
+          }
           path.skip();
         }
       }
