ef541a1bb36a7dfd7c401d1b50a615177e92e558
zhangjunqiu <zhangjunqiu@beisen.com>
2019-11-21 11:17:53 +0800

    fix(index)标识符的命名问题

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
 module.exports = function(babel) {
   var t = babel.types;
   return {
-    name: "babel-plugin-add-key-for-each-child",
+    name: 'babel-plugin-add-key-for-each-child',
     visitor: {
       CallExpression(path, state) {
-        var hasMap = path.node && path.node.callee && path.node.callee.property && path.node.callee.property.name === 'map';
-        var jsxPath = path.node && path.node.arguments && path.node.arguments[0] && path.node.arguments[0].body;
-        if (!!jsxPath) {
-          jsxPath = (path.node && path.node.arguments && path.node.arguments[0] && path.node.arguments[0].body && path.node.arguments[0].body.body && path.node.arguments[0].body.body.find(b => b.type === 'ReturnStatement') || {argument: false}).argument;
+        // 可能返回jsx，可能在函数体里返回jsx
+        var jsxNode = path.node && path.node.arguments && path.node.arguments[0] && path.node.arguments[0].body;
+        if (!t.isJSXElement(jsxNode)) {
+          jsxNode = (path.node && path.node.arguments && path.node.arguments[0] && path.node.arguments[0].body && path.node.arguments[0].body.body && path.node.arguments[0].body.body.find(b => b.type === 'ReturnStatement') || {}).argument;
         }
-        if (hasMap && !!jsxPath) {
+        // 是否map
+        var isMap = path.node && path.node.callee && path.node.callee.property && path.node.callee.property.name === 'map';
+        // 是否jsx
+        var isJsx = t.isJSXElement(jsxNode);
+        // 若满足map、jsx的条件
+        if (isMap && isJsx) {
+          // map参数
           var params = path.node.arguments[0].params;
-          var attrib = jsxPath.openingElement.attributes;
-          if (params.length < 2) {
-            params.push(t.identifier('i'));
+          // jsx属性
+          var attrib = jsxNode.openingElement.attributes;
+          var identi;
+          // map callback三个参数
+          if (params.findIndex(p => p.name === 'i') === -1) {
+            identi = 'i';
+          } else if (params.findIndex(p => p.name === 'j') === -1) {
+            identi = 'j';
+          } else if (params.findIndex(p => p.name === 'k') === -1) {
+            identi = 'k';
+          } else {
+            identi = 'l';
           }
-          if (attrib.findIndex(a => a.name && a.name.name === 'key') < 0) {
-            attrib.push(t.jsxAttribute(t.jsxIdentifier('key'), t.jsxExpressionContainer(t.identifier('i'))));
+          if (params.length > 1) {
+            identi = params[1].name;
+          } else {
+            params.push(t.identifier(identi));
           }
-          path.skip();
+          if (attrib.findIndex(a => a.name && a.name.name === 'key') === -1) {
+            attrib.push(t.jsxAttribute(t.jsxIdentifier('key'), t.jsxExpressionContainer(t.identifier(identi))));
+          }
         }
       }
     }
--- test/output.js
+++ test/output.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Com = props => {
   const num = [1, 2, 3];
   return <div>
-    {num.map(o => <Com num={o} />)}
+    {num.map((o, i) => <Com num={o} key={i} />)}
   </div>;
 };
 
